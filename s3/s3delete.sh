ea1dd0ceac416b51e5e4da22679ed74ba6f6ebfc
Jason Grimm <jasgrimm@cisco.com>
2018-04-01 18:24:36 -0400

    working on s3delete.sh

---
+++ s3/s3delete.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# Title		: s3create.sh
+# Description	: Lifecycle action script for creating AWS S3 buckets.
+# Author	: jasgrimm
+# Date		: 2018-04-01
+# Version	: 0.1
+# Usage		: bash s3create.sh
+# External Vars	: Read in at run time - $AWS_REGION, $AWS_ACCESS_KEY_ID, and $AWS_SECRET_ACCESS_KEY
+# Internal Vars	: Initialized within srcipt - $AWS_INSTALL_DIR, $AWS_CONFIG_DIR, $AWS_CONFIG_FILE, $AWS_CRED_FILE
+
+# If running as an "external-service", execute and terminate docker container on the orchestrator
+# . /utils.sh
+# print_log "$(env)"
+
+# If running within a virtual machine (default)
+. /usr/local/osmosix/etc/.osmosix.sh
+. /usr/local/osmosix/etc/userenv
+. /usr/local/osmosix/service/utils/cfgutil.sh
+. /usr/local/osmosix/service/utils/agent_util.sh
+
+# Declare / configure internal vars
+AWS_INSTALL_DIR="/usr/local/aws"
+PATH=$PATH:$AWS_INSTALL_DIR/bin
+AWS_CONFIG_DIR="/root/.aws"
+AWS_CONFIG_FILE="$AWS_CONFIG_DIR/config"
+AWS_CRED_FILE="$AWS_CONFIG_DIR/credentials"
+AWS_BUCKET_FILE_JSON="/root/bucketlist.json.txt"
+AWS_BUCKET_FILE_PRETTY="/root/bucketlist.pretty.txt"
+
+# Install prerequisites
+installPrerequisites() {
+	agentSendLogMessage "Installing prerequisites..."
+	if [ -f /bin/jq ]; then
+		agentSendLogMessage "JQ is already installed, skipping install."
+	else
+		agentSendLogMessage "JQ is not installed, installing now."
+		yum -y --skip-broken install jq
+	fi
+}
+
+# Functions
+installAWSCli() {
+    agentSendLogMessage "Installing AWS CLI tools..."
+
+        if [ -f $AWS_INSTALL_DIR/bin/aws ]; then
+            agentSendLogMessage  "AWS CLI already installed, skipping the AWS CLI Install."
+        else
+            mkdir -p $AWS_INSTALL_DIR; cd $AWS_INSTALL_DIR
+            wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip; unzip awscli-bundle.zip
+            ./awscli-bundle/install -i $AWS_INSTALL_DIR
+            agentSendLogMessage  "AWS CLI tools are now installed."
+        fi
+}
+
+configureAWSCli() {
+	agentSendLogMessage "Configuring AWS CLI tools..."
+
+	mkdir -p $AWS_CONFIG_DIR
+
+	echo "[default]" > $AWS_CONFIG_FILE
+	echo "region = $AWS_REGION" >> $AWS_CONFIG_FILE
+	chmod 600 $AWS_CONFIG_FILE
+
+	echo "[default]" > $AWS_CRED_FILE
+	echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> $AWS_CRED_FILE
+	echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> $AWS_CRED_FILE
+	chmod 600 $AWS_CRED_FILE
+}
+
+createAWSBucket() {
+	agentSendLogMessage "Creating AWS S3 Bucket: $AWS_INSTALL_DIR/bin/aws s3api create-bucket --bucket $AWS_BUCKET_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION"
+	$AWS_INSTALL_DIR/bin/aws s3api create-bucket --bucket $AWS_BUCKET_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
+	agentSendLogMessage "AWS S3 Bucket Create Complete."
+	sleep 10
+}
+
+installAWSCli
+configureAWSCli
+createAWSBucket
+
+agentSendLogMessage "** S3 Bucket Creation Service Complete **"
+
+exit 0
