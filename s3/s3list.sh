fd1713a2c644709b1b3f1ea1e5a6c3096a9d78d9
Jason Grimm <jasgrimm@cisco.com>
2018-04-01 02:54:00 -0400

    testing s3list

--- s3/s3list.sh
+++ s3/s3list.sh
@@ -1,98 +1,40 @@
 #!/bin/bash
-. /usr/local/osmosix/etc/.osmosix.sh
-. /usr/local/osmosix/etc/userenv
-. /usr/local/osmosix/service/utils/cfgutil.sh
-. /usr/local/osmosix/service/utils/agent_util.sh
+# Source Vars - $AWS_REGION, $AWS_ACCESS_KEY_ID, and $AWS_SECRET_ACCESS_KEY is passed in from
+# CloudCenter via a visible +/- editble or hidden paramater
+. /usr/local/osmosix/etc/.osmosix.sh; . /usr/local/osmosix/etc/userenv
+. /usr/local/osmosix/service/utils/cfgutil.sh; . /usr/local/osmosix/service/utils/agent_util.sh
+INSTALL_DIR="/usr/local/aws"
+export PATH=$PATH:$INSTALL_DIR/bin
 
-export SVCNAME=s3list
-export INSTALL_DIR="/usr/local/aws"
-export EC2_REGION="$region"
-
-agentSendLogMessage "Service Name :: $SVCNAME starting"
-
-# print and verify if access and secret key are defined as app or service parameters.
-agentSendLogMessage "AWS Region :: $EC2_REGION"
-agentSendLogMessage "AWS Secret Key :: $AWS_SECRET_KEY"
-agentSendLogMessage "AWS Access Key :: $AWS_ACCESS_KEY"
-
-# For Testing
-if [ -z $AWS_SECRET_KEY ]; then
-    agentSendLogMessage "AWS_SECRET_KEY not specified"
-    exit 1
-fi
+agentSendLogMessage "Service Name :: s3 Bucket List starting"
 
 installAWSCli() {
     agentSendLogMessage "Installing AWS CLI tools..."
         if [ type aws > /dev/null ]; then
-            agentSendLogMessage  "AWS Cli already installed skipping the AWS Cli Install";
-            export PATH=$PATH:$INSTALL_DIR/bin
-            agentSendLogMessage "PATH value is = $PATH"
+            agentSendLogMessage  "AWS CLI already installed skipping the AWS CLI Install";
         else
-            mkdir -p $INSTALL_DIR
-            cd $INSTALL_DIR
-            wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
-            unzip awscli-bundle.zip
+            mkdir -p $INSTALL_DIR; cd $INSTALL_DIR
+            wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip; unzip awscli-bundle.zip
             ./awscli-bundle/install -i $INSTALL_DIR
-            rm -f awscli-bundle.zip
-            export PATH=$PATH:$INSTALL_DIR/bin
-            agentSendLogMessage "PATH value is = $PATH"
         fi
 }
 
 configureAWSCli() {
-    agentSendLogMessage "Configuring AWS CLI..."
-        export JAVA_HOME="/usr/lib/jvm/jre"
-        export PATH=$PATH:$INSTALL_DIR/bin
-
-        if [ -z "$EC2_REGION" ]; then
-	    	export EC2_REGION="us-west-2"
-	    	export region="us-west-2"
-        fi
-
-        if [ ! -z "$CliqrCloud_EC2ARN" ]; then
-            credresult=`aws sts assume-role --role-arn "$CliqrCloud_EC2ARN" --role-session-name "MyRoleSessionName"`
-            if [ ! -n $credresult ]; then
-                agentSendLogMessage "Error in generating credentials using the role : $CliqrCloud_EC2ARN";
-                exit 1
-            fi
-
-        export AWS_SECRET_ACCESS_KEY=`echo $credresult | awk -F SecretAccessKey '{printf $2}' | awk -F \" '{printf $3}'`
-        export AWS_ACCESS_KEY_ID=`echo $credresult | awk -F AccessKeyId '{printf $2}' | awk -F \" '{printf $3}'`
-        export AWS_SECURITY_TOKEN=`echo $credresult | awk -F SessionToken '{printf $2}' | awk -F \" '{printf $3}'`
-
-        elif [ -z $CliqrCloudAccountPwd ] || [ -z $CliqrCloud_AccessSecretKey ]; then
-            agentSendLogMessage "Insufficient permissions to access the Cloud Account, contact your Admin for the Cloud Account Accessibility";
-            exit 1
-        else
-            export AWS_ACCESS_KEY_ID="$CliqrCloudAccountPwd"
-            export AWS_SECRET_ACCESS_KEY="$CliqrCloud_AccessSecretKey"
-        fi
-
-        if [ -z $AWS_ACCESS_KEY_ID ]; then
-            agentSendLogMessage "Cloud Account Access Key not found or couldn't generate with IAM role"
-            exit 1
-        fi
-
-        if [ -z $AWS_SECRET_ACCESS_KEY ]; then
-            agentSendLogMessage "Cloud Account Secret Key not found or couldn't generate with IAM role"
-            exit 1
-        fi
-
-        if [ -z $region ]; then
-            agentSendLogMessage "Region Value is not set"
-            exit 1
-        fi
-
-        export "AWS_DEFAULT_REGION"=$region
-        agentSendLogMessage "AWS Access Key :: $AWS_ACCESS_KEY_ID"
-        agentSendLogMessage "AWS Secret Key :: $AWS_SECRET_ACCESS_KEY"
-        agentSendLogMessage "AWS Region :: $AWS_DEFAULT_REGION"
+	agentSendLogMessage "Configuring AWS CLI tools..."
+	echo "[default]" > ~/.aws/config
+	echo "region = $AWS_REGION" >> ~/.aws/config
+	echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" > ~/.aws/credentials
+	echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
 }
 
-agentSendLogMessage "Executing service script"
-agentSendLogMessage "Install and configuring AWS CLI"
+listAWSBuckets() {
+	agentSendLogMessage "Listing AWS S3 Buckets..."
+	returnList=`aws s3api list-buckets`
+	agentSendLogMessage $returnList
+}
 
 installAWSCli
 configureAWSCli
-returnList=`aws s3 ls`
-agentSendLogMessage $returnList
+listAWSBuckets
+
+exit 0
