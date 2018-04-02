326561cfed7d3fdaea863114f7b05bb0b7f3ddf7
Jason Grimm <jasgrimm@cisco.com>
2018-04-02 18:07:41 -0400

    working on on init tests

--- tools/tools.sh
+++ tools/tools.sh
@@ -2,10 +2,14 @@
 agentSendLogMessage "Starting post-init stuff..."
 
 agentSendLogMessage "Adding cliqruser to /etc/sudoers..."
-echo "cliqruser  ALL= NOPASSWD: ALL" >> /etc/sudoers 
+echo "cliqruser  ALL= NOPASSWD: ALL" >> /etc/sudoers
+agentSendLogMessage "Last line of /etc/sudoers"
+agentSendLogMessage `tail -n 1 /etc/sudoers`
 
 agentSendLogMessage "Adding a new key to /home/$my_user/.ssh/authorized_keys..."
-echo $my_key >> /home/cliqruser/.ssh/authorized_keys
+echo $my_key >> /home/$my_user/.ssh/authorized_keys
 
 agentSendLogMessage "Appending /home/cliqruser/.ssh/authorized_keys to /home/centos/.ssh/authorized_keys..."
-echo "cliqruser  ALL= NOPASSWD: ALL" >> /etc/sudoers 
+echo "cliqruser  ALL= NOPASSWD: ALL" >> /etc/sudoers
+
+exit 0
