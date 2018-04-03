267624a504f4d0394361fcc6ee6c820243dedb63
Jason Grimm <jasgrimm@cisco.com>
2018-04-03 01:16:00 -0400

    working on on init tests

--- tools/tools.sh
+++ tools/tools.sh
@@ -12,8 +12,8 @@ sudo agentSendLogMessage "Adding cliqruser and centos users to sudoers..."
 sudo usermod -aG wheel centos; usermod -aG wheel cliqruser
 
 sudo agentSendLogMessage "Adding a new key to /home/cliqruser/.ssh/authorized_keys..."
-sudo echo "## Dynamically inserted key ##" >> /home/cliqruser/.ssh/authorized_keys
-sudo echo "%my_key%" >> /home/cliqruser/.ssh/authorized_keys
+echo "## Dynamically inserted key ##" >> /home/cliqruser/.ssh/authorized_keys
+echo "%my_key%" >> /home/cliqruser/.ssh/authorized_keys
 
 sudo agentSendLogMessage "Appending /home/cliqruser/.ssh/authorized_keys to /home/centos/.ssh/authorized_keys..."
 sudo bash -c "echo %my_key% >> /home/centos/.ssh/authorized_keys"
