5b911c9c09058653ee0e99ad14cd10f58a91e326
Jason Grimm <jasgrimm@cisco.com>
2018-04-03 01:11:34 -0400

    working on on init tests

--- tools/tools.sh
+++ tools/tools.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cat <<EOF >/home/cliqruser/post-init.sh
 # Source some cliqr variables and scripts
 sudo . /usr/local/osmosix/etc/.osmosix.sh
 sudo . /usr/local/osmosix/etc/userenv
@@ -12,18 +11,11 @@ sudo agentSendLogMessage "Starting post-init stuff..."
 sudo agentSendLogMessage "Adding cliqruser and centos users to sudoers..."
 sudo usermod -aG wheel centos; usermod -aG wheel cliqruser
 
-sudo agentSendLogMessage "Adding a new key to /home/$my_user/.ssh/authorized_keys..."
-sudo echo "## Dynamically inserted key ##" >> /home/$my_user/.ssh/authorized_keys
-sudo echo $my_key >> /home/$my_user/.ssh/authorized_keys
+sudo agentSendLogMessage "Adding a new key to /home/cliqruser/.ssh/authorized_keys..."
+sudo echo "## Dynamically inserted key ##" >> /home/cliqruser/.ssh/authorized_keys
+sudo echo "%my_key%" >> /home/cliqruser/.ssh/authorized_keys
 
 sudo agentSendLogMessage "Appending /home/cliqruser/.ssh/authorized_keys to /home/centos/.ssh/authorized_keys..."
-sudo echo "" >> /home/$centos/.ssh/authorized_keys
-sudo echo "## Copied over keys ##" >> /home/centos/.ssh/authorized_keys
-sudo cat /home/cliqruser/.ssh/authorized_keys >> /home/centos/.ssh/authorized_keys
-EOF
-
-cd /home/cliqruser
-chmod +x ./post-init.sh
-./post-init.sh
+sudo bash -c "echo %my_key% >> /home/centos/.ssh/authorized_keys"
 
 exit 0
